20f54577cc05bc8fcf443b8a652d300c0f932b07
Bartek Ciszkowski <bart.ciszk@gmail.com>
2019-01-30 14:56:16 -0500

    Reference correct hidden virtualenv

--- run-linty-fresh.sh
+++ run-linty-fresh.sh
@@ -35,7 +35,7 @@ main() {
 
     # Setup a virtual environment and install dependencies.
     python3 -m venv ./.ve
-    source ./ve/bin/activate
+    source ./.ve/bin/activate
     pip install -r requirements.txt
 
     echo "Linting!"
